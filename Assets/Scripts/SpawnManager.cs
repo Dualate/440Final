f71cf3c21be7f6b0fe7dbd61d4acdef490aee334
Dualate <cmgfranklin@gmail.com>
2024-05-08 00:17:28 -0400

    fixedmage_checkpoint
    -fixed mage prefab to animate properly
    -fixed spawner to prevent player from being overwhelmed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,8 +9,8 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] coinPrefab;
 
     //enemy spawn random location
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
+    private float spawnRangeX = 40;
+    private float spawnPosZ = 40;
     private float startDelayEnemy = 1;
     private float startDelayCoin = 2;
     public float spawnInterval = 1.5f;
@@ -22,17 +22,13 @@ public class SpawnManager : MonoBehaviour
         //InvokeRepeating("SpawnCoins", startDelayCoin, spawnInterval);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    
 
     void SpawnEnemy()
     {
         //Randomly generate enemy index and spawn position
         int enemyIndex = Random.Range(0, enemyPrefab.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, Random.Range(-spawnPosZ, spawnPosZ));
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX) + 10, .5f, Random.Range(-spawnPosZ, spawnPosZ) + 10);
 
         Instantiate(enemyPrefab[enemyIndex], spawnPos, enemyPrefab[enemyIndex].transform.rotation);
     }
